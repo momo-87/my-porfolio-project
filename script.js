749d7a3ffe8bb954484e14f8cbbade44e5018b3c
Momo-87 <momochristian87@gmail.com>
2023-12-12 02:12:30 +0100

    Implement text animation

--- script.js
+++ script.js
@@ -388,3 +388,60 @@ window.addEventListener('load', () => {
 });
 
 // text animation Start
+// function([string1, string2],target id,[color1,color2])
+
+const consoleText = (words, id, idMobile, colors) => {
+  if (colors === undefined) colors = ['#fff'];
+  let visible = true;
+  const con = document.getElementById('console');
+  const conMobile = document.getElementById('console-mobile');
+  let letterCount = 1;
+  let x = 1;
+  let waiting = false;
+  const target = document.getElementById(id);
+  const targetMobile = document.getElementById(idMobile);
+  target.setAttribute('style', `color:${colors[0]}`);
+  targetMobile.setAttribute('style', `color:${colors[0]}`);
+  window.setInterval(() => {
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount);
+      targetMobile.innerHTML = words[0].substring(0, letterCount);
+      window.setTimeout(() => {
+        const usedColor = colors.shift();
+        colors.push(usedColor);
+        const usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', `color:${colors[0]}`);
+        targetMobile.setAttribute('style', `color:${colors[0]}`);
+        letterCount += x;
+        waiting = false;
+      }, 1000);
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(() => {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000);
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount);
+      targetMobile.innerHTML = words[0].substring(0, letterCount);
+      letterCount += x;
+    }
+  }, 120);
+  window.setInterval(() => {
+    if (visible === true) {
+      con.className = 'console-underscore hidden';
+      conMobile.className = 'console-underscore hidden';
+      visible = false;
+    } else {
+      con.className = 'console-underscore';
+      conMobile.className = 'console-underscore';
+
+      visible = true;
+    }
+  }, 400);
+};
+consoleText(['Full-stack Developer.', 'JavaScript Developer.', 'React Developer.', 'Ruby on Rails Developer.', 'Next.js Developer.', 'Front-end Developer.', 'Back-end Developer.'], 'text', 'text-mobile', ['#9ba4b5', '#f7df1e', '#61dafb', '#CC0000', 'white', '#9ba4b5', '#9ba4b5']);
